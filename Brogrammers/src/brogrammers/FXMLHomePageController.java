978e6b5cd64f93e5244569afc171b4243ce808a0
Mauhib Iqbal <mauhib@gmail.com>
2017-10-28 15:39:01 -0500

    added homepage and channelpage

--- Brogrammers/src/brogrammers/FXMLHomePageController.java
+++ Brogrammers/src/brogrammers/FXMLHomePageController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author dc
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLHomePageController implements Initializable {
     
     @FXML
     private Label label;
