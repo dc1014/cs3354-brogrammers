7c8331cec66300ca9d2c64f60df27a7bdfd169a4
jay speights <jaymspeights@gmail.com>
2017-11-27 14:01:40 -0600

    parse functions now return response

---
+++ ChatroomAppServer/src/java/constants/Command.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class Command {
+    public static final String MESSAGE = "MESSAGE";
+    public static final String JOIN = "JOIN";
+    public static final String LEAVE = "LEAVE";
+    public static final String SETNAME = "SETNICKNAME";
+    public static final String LIST = "LIST";
+    
+    public static final String DELIM = "/";
+}
